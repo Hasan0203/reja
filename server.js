fe2109fa599f1e9e530b468a93e645451831b1a3
Hasan0203 <khasan0203@icloud.com>
2024-02-07 01:30:51 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 console.log("Web Serverni boshlash");
 
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+})
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -21,16 +33,15 @@ app.post("/create-item", (req, res) => {
     res.json({test: "success"});
 });
 
-app.get('/', function(req, res) {
+app.get('/author', function(req, res) {
+    res.render("author", {user: user});
+});
+
+app.get('/harid', function(req, res) {
     res.render("harid");
 });
 
-// app.get("/hello", function(req, res) {
-//     res.end(`<h1 style ="background: grey">HELLO WORLD by BekzodAli</h1>`);
-// });
-// app.get("/gift", function(req, res) {
-//     res.end(`<h1>Siz sovgalar sahifasidasiz</h1>`);
-// });
+
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
