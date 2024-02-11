c2cfcf2cece4bfe80f6f7472c7a4cbc2a20fdd01
Hasan0203 <khasan0203@icloud.com>
2024-02-11 21:35:23 +0900

    feat: MongoDB ulanish

--- server.js
+++ server.js
@@ -1,49 +1,26 @@
-console.log("Web Serverni boshlash");
-
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
 
-let user;
+const mongodb = require("mongodb");
 
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data);
+let db;
+const connectionString = "mongodb+srv://Kevin:upxu25GjRO5vxmr4@cluster0.fpjjdga.mongodb.net/Reja";
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    UseUnifiedTopology: true,
+}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function() {
+            console.log(
+                `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+            );
+        });
     }
-})
-
-// 1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2: Session code
-
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4: Rooting code
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
 });
 
-app.get('/author', function(req, res) {
-    res.render("author", {user: user});
-});
-
-app.get('/', function(req, res) {
-    res.render("reja");
-});
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
