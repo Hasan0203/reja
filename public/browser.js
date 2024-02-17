c176551cac91f526f58ac6cafaa3dc6a6060b7e0
Hasan0203 <khasan0203@icloud.com>
2024-02-17 10:24:51 +0900

    feat: Reja loyihamizning yakuniy bosqich logic

--- public/browser.js
+++ public/browser.js
@@ -1,5 +1,6 @@
 console.log("FrontEnd JS ishga tushdi");
 
+
 function itemTemplate(item) {
     return `<li 
     class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
@@ -59,7 +60,35 @@ document.addEventListener("click", function(e) {
         } 
     }
 
-    if(e.target.classList.contains("edit-me")) {
-        alert("Siz edit tugmasini bosdingiz");
+    //Edit-user
+    if (e.target.classList.contains("edit-me")) {
+        let userInput = prompt(
+            "O'zgartirish kiriting", 
+        e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+        );
+        if (userInput) {
+            axios
+            .post("/edit-item", {
+                id: e.target.getAttribute("data-id"),
+                new_input: userInput, 
+             })
+            .then((response) => {
+                console.log(response.data);
+                e.target.parentElement.parentElement.querySelector(
+                ".item-text"
+                ).innerHTML = userInput;
+            })
+            .catch((err) => {
+                console.log("Iltimos qaytadan harakat qiling!");
+            });
+        }
     }
 });
+
+
+document.getElementById("clean-all").addEventListener("click", function() {
+    axios.post("/delete-all", { delete_all: true }).then(respose => {
+        alert(respose.data.state);
+        document.location.reload();
+    })
+})
